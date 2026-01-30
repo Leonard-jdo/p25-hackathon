481ec8f21a660b743ef8f06c738a3af1f88c23f6
Leonard Joubert des Ouches <leonard.joubert_des_ouches@etu.minesparis.psl.eu>
2026-01-30 12:11:51 +0100

    fonctions points d'impact ajoutées à la classe car

--- car.py
+++ car.py
@@ -52,6 +52,38 @@ class Car :
 
         self.x=self.vx*dt+self.x
         self.y=self.vy*dt+self.y
+    
+    def get_contact_points(self):
+        contact_points = []
+        x = self.x
+        y = self.y
+        theta = self.theta
+        L = self.length
+        h = self.height
+
+        a = [(1,1),(1,-1),(-1,-1),(-1,1),(0,1),(1,0),(0,-1),(-1,0)]
+
+        for pt in a:
+            contactpoint = np.array([x + pt[0] * (L/2) * np.cos(theta) - pt[1] * (h/2) * np.sin(theta), y + pt[0] * (L/2) * np.sin(theta) + pt[1] * (h/2) * np.cos(theta)])
+            contact_points.append(contactpoint)
+        return(contact_points)
+
+
+    def get_normal_vectors(self):
+
+        normal_vectors=[]
+        x = self.x
+        y = self.y
+        pos = np.array(x,y)
+        theta = self.theta
+        L = self.length
+        h = self.height
+
+        for contactpoint in get_contact_points():
+            vector = contactpoint - pos
+            normal_vectors.append(vector/np.linalg.norm(vector))
+
+        return normal_vectors
 
         
 
