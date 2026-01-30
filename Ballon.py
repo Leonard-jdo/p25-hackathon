ea43e00938fe97f66bf31d7bd33ab44d6f988378
Valentin CHEILLAN <valentin.cheillan@etu.minesparis.psl.eu>
2026-01-30 11:18:08 +0100

    V4

--- Ballon.py
+++ Ballon.py
@@ -1,3 +1,7 @@
+from game import dt, y_sol, y_plafond
+alpha = 1e-3
+beta = 0.8
+g = 9,81
 class Ballon:
 
     def __init__(self, x, y, vx, vy,m,r):
@@ -15,7 +19,6 @@ class Ballon:
             return False
     
     def gravity_without_contact(self,alpha,g):
-        dt = 0.2 #implémentation méthode d'Euler 
         ax = (-alpha * self.vx) / self.m #frottements fluides uniquement
         ay = ((-alpha * self.vy) / self.m) + g  
         self.vx += ax * dt
@@ -25,22 +28,22 @@ class Ballon:
         self.corr_contact()
 
     def corr_contact(self, taille):
-        (bord1, bord2, plafond) = taille
+        (bord1, bord2, y_sol, y_plafond) = taille
         if self.x + self.r > bord2:#contact à droite
             self.x = bord2 - self.r  
-            self.vx = -self.vx 
+            self.vx = -self.vx * beta 
             
         elif self.x - self.r < bord1:#contact à gauche
             self.x = self.r + bord1               
-            self.vx = -self.vx 
+            self.vx = -self.vx * beta
             
-        if self.y - self.r < 0:#contact au sol
-            self.y = self.r               
-            self.vy = -self.vy 
+        if self.y - self.r < self.sol:#contact au sol
+            self.y = self.r + y_sol               
+            self.vy = -self.vy * beta 
 
-        if self.y + self.r > plafond:#contact mur 
-            self.y = plafond - self.r 
-            self.vy = -self.vy 
+        if self.y + self.r > y_plafond:#contact mur 
+            self.y = y_plafond - self.r 
+            self.vy = -self.vy * beta
 
     
 
