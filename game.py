e224fd49870f3d0f764fcdc7ecdfb64cca97073a
Chauvin Clément <clement.chauvin@etu.minesparis.psl.eu>
2026-01-30 11:16:30 +0100

    Update game.py

--- game.py
+++ game.py
@@ -2,18 +2,18 @@ class Game:
     background = pygame.image.load("background.jpg").convert()
     background = pygame.transform.scale(background, (800, 600))
 
-    ballon = pygame.image.load("ballon.png").convert_alpha()
-    car1 = pygame.image.load("car1.png").convert_alpha()
-    car2 = pygame.image.load("car2.png").convert_alpha()
+    #ballon = pygame.image.load("ballon.png").convert_alpha()
+    #car1 = pygame.image.load("car1.png").convert_alpha()
+    #car2 = pygame.image.load("car2.png").convert_alpha()
     
     def __init__(self):
         pygame.init()
         self.screen = pygame.display.set_mode((800, 600))
         self.clock = pygame.time.Clock()
         self.screen.blit(background, (0, 0))
-        self.screen.blit(ballon, (400, 300))
-        self.screen.blit(car1, (0, 0))
-        self.screen.blit(car2, (800, 0))
+        #self.screen.blit(ballon, (400, 300))
+        #self.screen.blit(car1, (0, 0))
+       # self.screen.blit(car2, (800, 0))
         pygame.display.flip()
 
 
@@ -27,6 +27,7 @@ class Game:
         clock.tick(60)
 
 
+Game()
 
 
 
